33f8e4762685135804e4e7f6539f3866dc469a1b
aray1 <ayushmanray@gmail.com>
2018-01-26 00:20:51 +0100

    Heroku runner

---
+++ src/main/java/net/aray/commonproblems/collectedrainwater/HerokuRunner.java
@@ -0,0 +1,18 @@
+package net.aray.commonproblems.collectedrainwater;
+
+import org.apache.tomee.embedded.Configuration;
+import org.apache.tomee.embedded.Container;
+
+import java.util.concurrent.CountDownLatch;
+
+import static java.lang.Integer.parseInt;
+
+public class HerokuRunner {
+    public static void main(final String[] args) throws InterruptedException {
+        try (Container container = new Container(
+                new Configuration().http(parseInt(System.getenv("PORT"))))
+                .deployClasspathAsWebApp("/", null)) {
+            new CountDownLatch(1).await();
+        }
+    }
+}
